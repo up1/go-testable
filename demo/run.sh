fadb0dce2c652712ecfd95c362ab255c7704db7e
somkiat <somkiat.p@gmail.com>
2018-07-06 00:27:55 +0700

    Add more test with fail case

--- demo/run.sh
+++ demo/run.sh
@@ -4,7 +4,8 @@ export GOPATH="$CURDIR"
 # go get gopkg.in/DATA-DOG/go-sqlmock.v1
 # go get github.com/stretchr/testify/assert
 gofmt -w src/
-go test -v --cover ./...
+go test -v -cover -coverprofile=coverage.out  ./...
+go tool cover -html=coverage.out
 go install main
 export GOPATH="$OLDGOPATH"
 echo 'finished'
--- demo/src/repository/mysql_account_test.go
+++ demo/src/repository/mysql_account_test.go
@@ -2,6 +2,7 @@ package repository_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
 	accountRepo "repository"
@@ -28,4 +29,47 @@ func TestGetByName(t *testing.T) {
 	// Assert
 	assert.NoError(t, err)
 	assert.NotNil(t, account)
+}
+
+func TestGetByNameWithDataNotFound(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	rows := sqlmock.NewRows([]string{"id", "name", "email"})
+	sql := `SELECT id, name, email FROM account WHERE name=?`
+
+	// Stub/Mocking
+	mock.ExpectQuery(sql).WillReturnRows(rows)
+	ar := accountRepo.NewMySQLAccountRepository(db)
+
+	// Act
+	account, err := ar.GetByName(context.TODO(), "Stub Name 1")
+
+	// Assert
+	assert.NoError(t, err)
+	assert.NotNil(t, account)
+}
+
+func TestGetByNameWithFail(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	sql := `SELECT id, name, email FROM account WHERE name=?`
+
+	// Stub/Mocking
+	mock.ExpectQuery(sql).WillReturnError(fmt.Errorf("some error"))
+	ar := accountRepo.NewMySQLAccountRepository(db)
+
+	// Act
+	account, err := ar.GetByName(context.TODO(), "Stub Name 1")
+
+	// Assert
+	assert.Error(t, err)
+	assert.Nil(t, account)
 }
